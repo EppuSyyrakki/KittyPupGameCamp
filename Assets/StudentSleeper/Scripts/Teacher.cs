3f9edc531f8bc6d85809e01542d647b24e3c2fde
Eppu Syyrakki <eppu.syyrakki@tuni.fi>
2020-06-01 16:20:08 +0300

    Testing discord bot
    Added script to teacher

--- Assets/StudentSleeper/Scripts/Teacher.cs
+++ Assets/StudentSleeper/Scripts/Teacher.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class Teacher : MonoBehaviour
 {
+    public GameObject[] boardPositions;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // testing discord bot   
     }
 
     // Update is called once per frame
