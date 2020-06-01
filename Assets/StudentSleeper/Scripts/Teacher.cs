0db5716fc70daf2dd07e50faac4d74f77c3d8018
Eppu Syyrakki <eppu.syyrakki@tuni.fi>
2020-06-01 17:06:45 +0300

    Teacher script timer added
    Teacher has property isWatching that goes true (and sprite turns red) when watching the player. Timer values are public.

--- Assets/StudentSleeper/Scripts/Teacher.cs
+++ Assets/StudentSleeper/Scripts/Teacher.cs
@@ -1,20 +1,61 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Teacher : MonoBehaviour
 {
+    public SpriteRenderer sr;
     public GameObject[] boardPositions;
+    public float _writingTimer;
+    private float _actualWritingTimer;
+    public float _watchingTimer;
+    private float _actualWatchingTimer;
+    public bool isWatching { get; set; }
+    private float _currentTime;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        // testing discord bot   
+        TeacherEnter();     
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        _currentTime += Time.deltaTime;
+
+        if (_currentTime >= _actualWritingTimer && !isWatching) TeacherWatching();
+
+        if (_currentTime >= _actualWatchingTimer && isWatching) TeacherNotWatching(); 
+
+        // Debug.Log("Timer is at " + _currentTime);
+    }
+
+    private void TeacherWatching()
+    {
+        _actualWatchingTimer = RandomizeTime(_watchingTimer);
+        sr.color = Color.red;   // TODO actual turning, warning sign, talking sound
+        isWatching = true;
+        _currentTime = 0;
+    }
+
+    private void TeacherNotWatching()
+    {
+        _actualWritingTimer = RandomizeTime(_writingTimer);
+        sr.color = Color.white; // TODO actual turning, writing, talking & writing sounds
+        isWatching = false;
+        _currentTime = 0;
+    }
+
+    private void TeacherEnter()
+    {
+        isWatching = true;
+        _currentTime = 0;
+        // teacher moves in from the left, watching and talking
+    }
+
+    private float RandomizeTime(float time)
+    {
+        return time;
+        // TODO randomization +-20%
     }
 }
