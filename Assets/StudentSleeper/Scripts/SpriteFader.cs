b595f40090b7a4c25271442a403ba333e0434865
Eppu Syyrakki <eppu.syyrakki@tuni.fi>
2020-06-02 11:49:32 +0300

    Teacher script update, prefabs
    Teacher does warning before turning. Also moves and createss writing on the board.

---
+++ Assets/StudentSleeper/Scripts/SpriteFader.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteFader : MonoBehaviour
+{
+    public bool fadingIn { get; set; }
+    public bool fadingOut { get; set; }
+    public float fadeSpeed = 5f;
+    private float _t = 0;
+    public SpriteRenderer sr;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (fadingIn)
+        {
+            if (_t <= 1) _t += Time.deltaTime * fadeSpeed;
+            fadingIn = Fade(0, 1);
+        }
+
+        if (fadingOut)
+        {
+            if (_t <= 1) _t += Time.deltaTime * fadeSpeed;
+            fadingOut = Fade(1, 0);
+
+            if (!fadingOut)
+            {
+                Destroy(gameObject);    // add GameObject.Destroy if doesn't destroy
+            }
+        }
+    }
+
+    private bool Fade(int start, int stop)
+    {
+        Color whiteAlpha = new Color(1, 1, 1, Mathf.Lerp(start, stop, _t));
+        sr.color = whiteAlpha;
+
+        if (_t >= 1) {_t = 0; return false;}
+        else return true;
+    }
+}
--- Assets/StudentSleeper/Scripts/Teacher.cs
+++ Assets/StudentSleeper/Scripts/Teacher.cs
@@ -4,58 +4,141 @@ public class Teacher : MonoBehaviour
 {
     public SpriteRenderer sr;
     public GameObject[] boardPositions;
-    public float _writingTimer;
-    private float _actualWritingTimer;
+    public GameObject writing;
+    public float _walkSpeed;
+    public float _writingTimer;   
     public float _watchingTimer;
     private float _actualWatchingTimer;
-    public bool isWatching { get; set; }
+    private float _actualWritingTimer;
     private float _currentTime;
+    private float _lerpT;
+    private int _boardIndex;
+    public bool isWatching { get; set; }
+    public bool isReady { get; set; }
+    private Vector3 lastPosition;
+    private Vector3 nextPosition;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        TeacherEnter();     
+        lastPosition = transform.position;
+        nextPosition = transform.position;
+        _boardIndex = 0;
+        Enter();     
     }
 
     // Update is called once per frame
     void Update()
     {
+        Debug.Log("boardIndex is " + _boardIndex);
+
+        if (_currentTime >= _actualWritingTimer * 0.9f && !isWatching) WatchingSoon();
+
+        if (_currentTime >= _actualWritingTimer && !isWatching) Watching();
+
+        if (_currentTime >= _actualWatchingTimer && isWatching) NotWatching();
+
         _currentTime += Time.deltaTime;
 
-        if (_currentTime >= _actualWritingTimer && !isWatching) TeacherWatching();
-
-        if (_currentTime >= _actualWatchingTimer && isWatching) TeacherNotWatching(); 
-
-        // Debug.Log("Timer is at " + _currentTime);
+        if (transform.position.x != nextPosition.x) Walk();
+        else _lerpT = 0;
     }
 
-    private void TeacherWatching()
+    private void Enter()
     {
+        // teacher moves in from the left, watching and talking
+        Watching();
+    }
+
+    private void Walk()
+    {
+        transform.position = Vector3.Lerp(lastPosition, nextPosition, _lerpT);
+        _lerpT += Time.deltaTime * _walkSpeed;
+    }
+
+    private void Watching()
+    {       
         _actualWatchingTimer = RandomizeTime(_watchingTimer);
+        // Debug.Log("Watching time is " + _actualWatchingTimer);
+
         sr.color = Color.red;   // TODO actual turning, warning sign, talking sound
         isWatching = true;
         _currentTime = 0;
     }
 
-    private void TeacherNotWatching()
+    private void WatchingSoon()
     {
+        sr.color = Color.yellow;
+    }
+
+    private void NotWatching()
+    {
+        DefineVectors();
         _actualWritingTimer = RandomizeTime(_writingTimer);
+        // Debug.Log("Writing time is " + _actualWritingTimer);
+
+        SpawnWriting();
         sr.color = Color.white; // TODO actual turning, writing, talking & writing sounds
         isWatching = false;
         _currentTime = 0;
     }
 
-    private void TeacherEnter()
-    {
-        isWatching = true;
-        _currentTime = 0;
-        // teacher moves in from the left, watching and talking
-    }
-
     private float RandomizeTime(float time)
     {
-        return time;
-        // TODO randomization +-20%
+        return Random.Range(0.8f * time, 1.2f * time);
+    }
+
+    private void SpawnWriting()
+    {
+        if (_boardIndex < 15)
+        {
+            GameObject writingClone = Instantiate(writing, boardPositions[_boardIndex].transform);
+            SpriteFader sf = writingClone.GetComponent<SpriteFader>();
+            SpriteRenderer sr = writingClone.GetComponent<SpriteRenderer>();
+            sr.flipY = RandomFlip();
+            sr.flipX = RandomFlip();
+            sf.fadingIn = true;
+            _boardIndex++;
+        } 
+        else
+        {
+            GameObject[] allWritings = GameObject.FindGameObjectsWithTag("Writing");
+
+            for (int i = 0; i < allWritings.Length; i++)
+            {
+                SpriteFader sf = allWritings[i].GetComponent<SpriteFader>();
+                sf.fadingOut = true;
+            }
+            _boardIndex = 0;
+            return;
+        }
+    }
+
+    private bool RandomFlip()
+    {
+        float number = Random.Range(0f, 1f);
+        if (number >= 0.49999) return true;
+        else return false;
+    }
+
+    private void DefineVectors()
+    {
+        float lastX;
+        float nextX;
+
+        if (_boardIndex + 1 < boardPositions.Length && _boardIndex > 0) 
+        { 
+            lastX = boardPositions[_boardIndex - 1].transform.position.x;
+            nextX = boardPositions[_boardIndex].transform.position.x;
+        }
+        else
+        {
+            lastX = boardPositions[boardPositions.Length - 1].transform.position.x;
+            nextX = boardPositions[0].transform.position.x;
+        }
+
+        lastPosition = new Vector3(lastX, transform.position.y, transform.position.z);
+        nextPosition = new Vector3(nextX, transform.position.y, transform.position.z);
     }
 }
