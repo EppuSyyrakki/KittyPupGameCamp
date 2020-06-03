f6832774966e56b2cfecc02a7e2c85d46da4789d
Terhi <salonenterhi19@gmail.com>
2020-06-03 16:12:47 +0300

    z to bubble - work in process

---
+++ Assets/StudentSleeper/Scripts/MarkZ.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MarkZ : MonoBehaviour
+{
+    public GameObject _scoreMarkerObject;
+    public Rigidbody _rigidBody;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidBody.useGravity = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+   
+}
