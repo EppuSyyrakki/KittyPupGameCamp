2156549815d58343958d548ff0273cbf2c0c8a81
Terhi <salonenterhi19@gmail.com>
2020-06-18 14:39:15 +0300

    Added match Disqualified-er

--- Assets/Wrestling/Scripts/DrawTrigger.cs
+++ Assets/Wrestling/Scripts/DrawTrigger.cs
@@ -1,22 +1,64 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Wrestling;
 
 public class DrawTrigger : MonoBehaviour
 {
-    UIMaster uiMaster;
+    [SerializeField]
+    private GameObject textObj;
+
+    private List<int> scoresBegin;
+    private List<int> scoresEnd;
+    private int scoreOne;
+    private int scoreTwo;
+    private bool _isTapOut { get; set; }
+
     private void Start()
     {
-        uiMaster = GameObject.Find("Canvas In Game").GetComponent<UIMaster>();
+        
+        InitTrigger();
+    }
+
+    private void InitTrigger()
+    {
+        textObj.SetActive(false);
+        scoresBegin = new List<int>();
+        scoresEnd = new List<int>();
+        UpdateScores(scoresBegin);
     }
 
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player1" || other.gameObject.tag == "Player2")
         {
-            Debug.Log("Round ends in draw");
-            // uiMaster.TriggerDrawTextOrWhatever();
+
+            InitScoresEnd();
+            GetIsTapOut();
+
+            if (!_isTapOut) textObj.SetActive(true);
         }
+    }
+
+    private void GetIsTapOut()
+    {
+        for (int i = 0; i < 2; i++)
+        {
+            if (scoresBegin[i] != scoresEnd[i]) _isTapOut = true;
+        }
+    }
+
+    private void InitScoresEnd()
+    {
+        UpdateScores(scoresEnd);
+    }
+
+    private void UpdateScores(List<int> list)
+    {
+        scoreOne = ScoreKeeper._playerOneScore;
+        scoreTwo = ScoreKeeper._playerTwoScore;
+        list.Add(scoreOne);
+        list.Add(scoreTwo);
     }
 }
