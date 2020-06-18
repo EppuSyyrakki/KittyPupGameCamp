8c74060d3062b0e92902e0917d954130556383fc
Eppu Syyrakki <eppu.syyrakki@tuni.fi>
2020-06-18 15:23:01 +0300

    Audio scripts and clips added

---
+++ Assets/Wrestling/Scripts/MainAudio.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UIElements;
+
+public class MainAudio : MonoBehaviour
+{
+    // public static int masterVolume;
+    // public SliderInt masterSlider;
+
+    [SerializeField] private AudioSource musicSource;
+    [SerializeField] private AudioSource effectSource;
+    [SerializeField] private AudioClip startRound;
+    [SerializeField] private AudioClip endRound;
+
+    // private AudioMixerGroup musicChannel;
+    // private AudioMixerGroup effectsChannel;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Wrestling/Scripts/PlayerAudio.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAudio : MonoBehaviour
+{
+    private AudioSource audioSource;
+
+    [SerializeField] AudioClip[] contact;
+    [SerializeField] AudioClip[] death;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = gameObject.GetComponent<AudioSource>();
+    }
+
+    
+}
