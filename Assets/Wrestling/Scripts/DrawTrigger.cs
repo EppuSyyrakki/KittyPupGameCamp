8a71ddd61a4a3ed051607c827e9a4184fa2770ee
Eppu Syyrakki <eppu.syyrakki@tuni.fi>
2020-06-18 11:25:19 +0300

    Trigger for draw added

---
+++ Assets/Wrestling/Scripts/DrawTrigger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Wrestling;
+
+public class DrawTrigger : MonoBehaviour
+{
+    UIMaster uiMaster;
+    private void Start()
+    {
+        uiMaster = GameObject.Find("Canvas In Game").GetComponent<UIMaster>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player1" || other.gameObject.tag == "Player2")
+        {
+            Debug.Log("Round ends in draw");
+            // uiMaster.TriggerDrawTextOrWhatever();
+        }
+    }
+}
